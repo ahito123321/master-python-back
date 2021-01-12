565c898be671736d681ee3b4626c704d0bcf028c
ahito123321 <ahito.123321@gmail.com>
2021-01-12 18:30:37 +0300

    Added features

--- python_back/settings.py
+++ python_back/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'route',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'python_back.urls'
@@ -124,3 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
+CORS_ORIGIN_REGEX_WHITELIST = (
+    'http://localhost:3000',
+)
--- route/route/views.py
+++ route/route/views.py
@@ -10,7 +10,7 @@ GOOGLE_API_KEY = 'AIzaSyB8pHk0GskSMHDLLYPEC1-wdHXoUDhlDwc'
 GOOGLE_LANGUAGE = 'ru'
 GOOGLE_DISTANCE_MATRIX_END_POINT = 'https://maps.googleapis.com/maps/api/distancematrix/json'
 
-BORDER_BOUND = 'http://127.0.0.1:5001/api/bab/basic'
+BORDER_BOUND = 'https://mbab.herokuapp.com/api/bab/basic'
 
 RANDOM_CONSISTENCY_SIZE = [0, 0, 0.58, 0.9, 1.12]
 
@@ -23,6 +23,7 @@ class Point:
 
 
 class Route:
+
     def __init__(self, origin_point, destination_point, mode='DRIVING'):
         self.distance = 0
         self.duration = 0
@@ -46,10 +47,6 @@ class Route:
             headers={'Content-type': 'application/json'}
         )
         json_response = response.json()
-
-        print('\nget_instance')
-        print('rows {0}'.format(json_response['rows'].__len__()))
-        print('elements {0}'.format(json_response['rows'][0]['elements'].__len__()))
         self.distance = json_response['rows'][0]['elements'][0]['distance']['value']
         self.duration = json_response['rows'][0]['elements'][0]['duration']['value']
 
@@ -71,7 +68,6 @@ class Route:
                 existed.remove(id)
 
                 Route._init_routes(full_routes, new_route, existed)
-                print(new_route)
             index += 1
 
         return full_routes
@@ -90,6 +86,65 @@ class Route:
             existed_copy.remove(id)
 
             Route._init_routes(full_routes, selected_copy, existed_copy)
+
+
+class ComboRoute:
+    def __init__(self, full_route, json_settings):
+        self.full_route = full_route
+        self.prepared_route = list()
+        self.route_duration = 0
+        self.route_distance = 0
+        self.routes_price = 0
+        self.route_full_duration = 0
+        self.json_settings = json_settings
+
+    def init_route(self, points, routes):
+        for index in range(points.__len__() - 1):
+
+            origin_id = points[index]
+            destination_id = points[index + 1]
+            temp = list()
+            temp.append(origin_id)
+            temp.append(destination_id)
+            self.prepared_route.append(temp)
+
+            for route in routes:
+                if route.origin_point.id == origin_id and route.destination_point.id == destination_id:
+                    self.route_duration += route.duration
+                    self.route_distance += route.distance
+
+                    if self.json_settings['takeRoutePrice']:
+                        car_consumption = self.json_settings['carConsumption']
+                        fuel_cost = self.json_settings['fuelCost']
+                        self.routes_price += route.distance * (car_consumption / (100 * 1000)) * fuel_cost
+                    break
+
+    def getRouteForResponse(self):
+        routes = list()
+        route = list()
+        for point in self.full_route:
+            route.append(point)
+
+            if route.__len__() == 2:
+                routes.append(route)
+                route = list()
+                route.append(point)
+
+        return {
+            'route': routes,
+            'duration': {
+                'label': 'Время',
+                'value': self.route_duration
+            },
+            'distance': {
+                'label': 'Расстояние',
+                'value': self.route_distance
+            },
+            'price': {
+                'label': 'Цена',
+                'value': self.routes_price
+            }
+        }
 
 
 class SaatiMatrix:
@@ -147,9 +202,11 @@ class RouteAlgorithm:
         id_starting_point = 0
         index_starting_point = 0
         index = 0
+        initial_point = list()
 
         for json_point in json_points:
             origin_point = Point(json_point['position']['lat'], json_point['position']['lng'], json_point['id'])
+            initial_point.append(origin_point)
 
             if json_point['isStartingPoint']:
                 id_starting_point = json_point['id']
@@ -173,8 +230,11 @@ class RouteAlgorithm:
         duration_matrix = [['' for j in range(number_of_points)] for i in range(number_of_points)]
         distance_matrix = [['' for j in range(number_of_points)] for i in range(number_of_points)]
         index = 0
+        initial_ids = list()
 
         for row in range(number_of_points):
+            if index == 0:
+                initial_ids.append(routes[index].origin_point)
             for col in range(number_of_points):
                 if row != col:
                     routes[index].init_distances()
@@ -202,67 +262,113 @@ class RouteAlgorithm:
             headers={'Content-type': 'application/json'}
         )
 
+        duration_response_json = duration_response.json()
+        distance_response_json = distance_response.json()
+        duration_response_json_route = duration_response.json()['route']
+        distance_response_json_route = distance_response.json()['route']
+
+        print(duration_response_json)
+        print(distance_response_json)
+
+        for ik in range(number_of_points):
+            first_point_id_dur = duration_response_json_route[ik][0]
+            second_point_id_dur = duration_response_json_route[ik][1]
+            first_point_id_dis = distance_response_json_route[ik][0]
+            second_point_id_dis = distance_response_json_route[ik][1]
+
+            for jk in range(number_of_points):
+                if first_point_id_dur == jk:
+                    duration_response_json_route[ik][0] = initial_point[jk].id
+                if first_point_id_dur == jk:
+                    duration_response_json_route[ik][1] = initial_point[jk].id
+                if first_point_id_dis == jk:
+                    distance_response_json_route[ik][0] = initial_point[jk].id
+                if second_point_id_dis == jk:
+                    distance_response_json_route[ik][1] = initial_point[jk].id
+
         return {
-            'duration': duration_response.json(),
-            'distance': distance_response.json()
+            'success': True,
+            'routes': {
+                'duration': duration_response.json(),
+                'distance': distance_response.json()
+            },
+            'labels': {
+                'duration': 'Время',
+                'distance': 'Расстояние'
+            }
         }
 
     @staticmethod
     def call_sati(json_points, json_settings, json_prioritization):
-        if json_prioritization['priceToTime']:
-            temp = np.array([
-                [1, 1 / json_prioritization['distanceToTime'], 1 / json_prioritization['priceToTime']],
-                [json_prioritization['distanceToTime'], 1, 1 / json_prioritization['priceToDistance']],
-                [json_prioritization['priceToTime'], json_prioritization['priceToDistance'], 1]
-            ])
-            prioritization_matrix = SaatiMatrix(temp)
-            print(prioritization_matrix.norm_vector)
-            print(prioritization_matrix.l_max)
-            print(prioritization_matrix.consistency_relation)
+        temp = np.array([
+            [1, 1 / json_prioritization['distanceToTime'], 1 / json_prioritization['priceToTime']],
+            [json_prioritization['distanceToTime'], 1, 1 / json_prioritization['priceToDistance']],
+            [json_prioritization['priceToTime'], json_prioritization['priceToDistance'], 1]
+        ])
+        prioritization_matrix = SaatiMatrix(temp)
 
-            if prioritization_matrix.consistency_relation > 0.1:
-                return {
-                    'message': 'Неверные параметры приоритизации! Проверьте введённые данные!'
-                }
+        if prioritization_matrix.consistency_relation > 0.1:
+            return {
+                'success': False,
+                'message': 'Неверные параметры приоритизации! Проверьте введённые данные!'
+            }
 
-            routes = list()
-            index = 0
-            id_starting_point = 0
-            index_starting_point = 0
-            points_ids = list()
+        routes = list()
+        index = 0
+        index_starting_point = 0
+        points_ids = list()
 
-            for json_point in json_points:
-                origin_point = Point(json_point['position']['lat'], json_point['position']['lng'], json_point['id'])
-                points_ids.append(origin_point.id)
+        for json_point in json_points:
+            origin_point = Point(json_point['position']['lat'],
+                                 json_point['position']['lng'],
+                                 json_point['id'])
+            points_ids.append(origin_point.id)
+
+            if json_point['isStartingPoint']:
+                id_starting_point = json_point['id']
+                index_starting_point = index
+
+            for json_another_point in json_point['anotherPoints']:
+                destination_point_id = json_another_point['id']
+                route = Route(origin_point, json_another_point['variant'])
 
                 if json_point['isStartingPoint']:
-                    id_starting_point = json_point['id']
-                    index_starting_point = index
+                    route.isStartRoute = True
 
-                for json_another_point in json_point['anotherPoints']:
-                    destination_point_id = json_another_point['id']
-                    route = Route(origin_point, json_another_point['variant'])
+                for _json_point in json_points:
+                    if _json_point['id'] == destination_point_id:
+                        route.destination_point = Point(_json_point['position']['lat'],
+                                                        _json_point['position']['lng'],
+                                                        _json_point['id'])
 
-                    if json_point['isStartingPoint']:
-                        route.isStartRoute = True
+                routes.append(route)
 
-                    for _json_point in json_points:
-                        if _json_point['id'] == destination_point_id:
-                            route.destination_point = Point(_json_point['position']['lat'],
-                                                            _json_point['position']['lng'],
-                                                            _json_point['id'])
+            index += 1
 
-                    routes.append(route)
+        full_initial_routes = Route.get_routes(index_starting_point, points_ids)
 
-                index += 1
+        for route in routes:
+            route.init_distances()
 
-            print(points_ids)
-            print(index_starting_point)
-
-            full_routes = Route.get_routes(index_starting_point, points_ids)
+        full_routes = list()
+        print('\nroutes')
+        for full_initial_route in full_initial_routes:
+            comboRoute = ComboRoute(full_initial_route, json_settings)
+            comboRoute.init_route(full_initial_route, routes)
+            full_routes.append(comboRoute)
+            print('route', comboRoute.full_route)
+            print('distance', comboRoute.route_distance)
+            print('duration', comboRoute.route_duration)
+            print('duration', comboRoute.routes_price)
 
         return {
-            'test': 'test'
+            'success': True,
+            'routes': {
+                'main': full_routes[0].getRouteForResponse()
+            },
+            'labels': {
+                'main': 'Основной'
+            }
         }
 
 
@@ -271,7 +377,7 @@ class RouteView(APIView):
         return Response('get')
 
     def post(self, request):
-        try:
+        # try:
             json_body = json.loads(request.body)
             json_points = json_body['points']
             json_settings = json_body['settings']
@@ -280,12 +386,11 @@ class RouteView(APIView):
             print(json_settings)
             print(json_prioritization)
 
-            if (json_settings['takeRating'] or
-                    json_settings['takeWorkingHours'] or
-                    json_settings['takeRoutePrice'] or
-                    json_settings['takePrioritization']):
+            if (json_settings['takePrioritization'] == True):
+                print('SAATI')
                 result = RouteAlgorithm.call_sati(json_points, json_settings, json_prioritization)
             else:
+                print('BAB')
                 result = RouteAlgorithm.call_branch_and_bound_method(json_points)
 
             # print(json_points)
@@ -295,9 +400,10 @@ class RouteView(APIView):
                 'success': True,
                 'data': result
             })
-        except Exception as err:
-            print(err)
-            return Response({
-                'success': False,
-                'message': 'Ошибка! Проверьте пожалуйста выбранные точки!'
-            })
+        # except Exception as err:
+        #     print('err')
+        #     print(err)
+        #     return Response({
+        #         'success': False,
+        #         'message': 'Ошибка! Проверьте пожалуйста выбранные точки!'
+        #     })
